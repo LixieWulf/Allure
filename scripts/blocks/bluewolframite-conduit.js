a4168a566304433d93f4dd7d7a1ed6795b675d75
MEEP of Faith <meepmatthew@gmail.com>
2020-04-02 23:32:26 -0700

    hmst

--- scripts/blocks/bluewolframite-conduit.js
+++ scripts/blocks/bluewolframite-conduit.js
@@ -1,9 +1,9 @@
 const bluewolframiteConduit = extendContent(Conduit, "bluewolframite-conduit", {
-    load(){
+    /*load(){
         this.super$load()
         for(int i = 0; i < this.topRegions.length; i++){
             this.topRegions[i] = Core.atlas.find(this.name + "-top-" + i);
             this.botRegions[i] = Core.atlas.find(this.name + "-bottom-" + i);
         }
-    }
+    }*/
 });
