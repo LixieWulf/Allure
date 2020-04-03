540d75e3f77ed7b332ce8761fb4a6638cf38929a
MEEP of Faith <meepmatthew@gmail.com>
2020-04-02 22:50:12 -0700

    test 3

--- scripts/blocks/bluewolframite-conduit.js
+++ scripts/blocks/bluewolframite-conduit.js
@@ -6,6 +6,11 @@ const bluewolfConduit = extendContent(Conduit, "bluewolframite-conduit", {
     },
     
     draw(tile){
-        Draw.rect(botRegions[bits[0]], req.drawx(), req.drawy(), botRegions[bits[0]].getWidth() * Draw.scl * req.animScale, botRegions[bits[0]].getHeight() * Draw.scl * req.animScale, req.rotation * 90);
+        Draw.colorl(0.34f);
+        Draw.alpha(0.5f);
+        Draw.rect(botRegions[bits[0]], tile.drawx(), tile.drawy(), botRegions[bits[0]].getWidth() * Draw.scl * tile.animScale, botRegions[bits[0]].getHeight() * Draw.scl * tile.animScale, tile.rotation * 90);
+        Draw.color();
+        
+        Draw.rect(topRegions[bits[0]], tile.drawx(), tile.drawy(), topRegions[bits[0]].getWidth() * Draw.scl * tile.animScale, topRegions[bits[0]].getHeight() * Draw.scl * tile.animScale, tile.rotation * 90);
     }
 });
