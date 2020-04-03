8b5d3234abd4eaba3d6e80cc2cfac4e276153449
MEEP of Faith <meepmatthew@gmail.com>
2020-04-02 22:51:51 -0700

    Sensical suggestion

--- scripts/blocks/bluewolframite-conduit.js
+++ scripts/blocks/bluewolframite-conduit.js
@@ -1,6 +1,7 @@
 const bluewolfConduit = extendContent(Conduit, "bluewolframite-conduit", {
     load(){
         for(int i = 0; i < topRegions.length; i++){
+            topRegions[i] = Core.atlas.find(this.name + "-top-" + i);
             botRegions[i] = Core.atlas.find(this.name + "-bottom-" + i);
         }
     },
@@ -8,9 +9,9 @@ const bluewolfConduit = extendContent(Conduit, "bluewolframite-conduit", {
     draw(tile){
         Draw.colorl(0.34f);
         Draw.alpha(0.5f);
-        Draw.rect(botRegions[bits[0]], tile.drawx(), tile.drawy(), botRegions[bits[0]].getWidth() * Draw.scl * tile.animScale, botRegions[bits[0]].getHeight() * Draw.scl * tile.animScale, tile.rotation * 90);
+        Draw.rect(this.botRegions[bits[0]], tile.drawx(), tile.drawy(), this.botRegions[bits[0]].getWidth() * Draw.scl * tile.animScale, this.botRegions[bits[0]].getHeight() * Draw.scl * tile.animScale, tile.rotation * 90);
         Draw.color();
         
-        Draw.rect(topRegions[bits[0]], tile.drawx(), tile.drawy(), topRegions[bits[0]].getWidth() * Draw.scl * tile.animScale, topRegions[bits[0]].getHeight() * Draw.scl * tile.animScale, tile.rotation * 90);
+        Draw.rect(this.topRegions[bits[0]], tile.drawx(), tile.drawy(), this.topRegions[bits[0]].getWidth() * Draw.scl * tile.animScale, this.topRegions[bits[0]].getHeight() * Draw.scl * tile.animScale, tile.rotation * 90);
     }
 });
