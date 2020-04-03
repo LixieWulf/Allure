d091cf6827834e09cd5479b8023ceca12369a1d6
MEEP of Faith <meepmatthew@gmail.com>
2020-04-02 22:44:39 -0700

    a little help from #scripting

--- scripts/blocks/bluewolframite-conduit.js
+++ scripts/blocks/bluewolframite-conduit.js
@@ -1,11 +1,11 @@
-const bluewolframiteConduit = extendContent(Conduit, "bluewolframite-conduit", {
+const bluewolfConduit = extendContent(Conduit, "bluewolframite-conduit", {
     load(){
-        super.load();
-
-        liquidRegion = Core.atlas.find("conduit-liquid");
         for(int i = 0; i < topRegions.length; i++){
-            topRegions[i] = Core.atlas.find(name + "-top-" + i);
-            botRegions[i] = Core.atlas.find(name + "-bottom-" + i);
+            botRegions[i] = Core.atlas.find(this.name + "-bottom-" + i);
         }
+    },
+    
+    draw(tile){
+        Draw.rect(botRegions[bits[0]], req.drawx(), req.drawy(), botRegions[bits[0]].getWidth() * Draw.scl * req.animScale, botRegions[bits[0]].getHeight() * Draw.scl * req.animScale, req.rotation * 90);
     }
 });
