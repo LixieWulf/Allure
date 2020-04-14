095020b436db9b756dfa7413c546b0c33f7fe3e1
MEEP of Faith <meepmatthew@gmail.com>
2020-04-14 10:10:06 -0700

    So what happens if I comment out the draw stuff...

--- scripts/blocks/solar-flare.js
+++ scripts/blocks/solar-flare.js
@@ -4,8 +4,6 @@ const solarflare = new LaserTurret("solar-flare");
 solarflare.consumes.add(new ConsumeLiquidFilter(boolf(liquid=>liquid.temperature<=0.5&&liquid.flammability<0.1),/*amount per tick*/0.5)).update(false);
 
 var tmpColor = new Color();
-var tmpColorfish = new Color();
-var tmpColoregg = new Color();
 var colors = [Color.valueOf("e6c04555"), Color.valueOf("f7d95eaa"), Color.valueOf("ffec6e"), Color.white];
 var tscales = [1, 0.7, 0.5, 0.2];
 var strokes = [2, 1.5, 1, 0.3];
@@ -43,12 +41,12 @@ solarflare.shootType = extend(BasicBulletType, {
         baseLen = (length) * b.fout();
 
         Lines.lineAngle(b.x,b.y,b.rot(),baseLen);
-        /*Lines.lineAngle(b.x, b.y, b.rot() + 8,baseLen);
-        Lines.lineAngle(b.x, b.y, b.rot() - 8,baseLen);*/
+        Lines.lineAngle(b.x, b.y, b.rot() + 8,baseLen);
+        Lines.lineAngle(b.x, b.y, b.rot() - 8,baseLen);
         for(var s = 0; s < colors.length; s++){
             Draw.color(tmpColor.set(colors[s]).mul(1+Mathf.absin(Time.time(),1,0.1)));
             for(var i = 0; i < tscales.length; i++){
-                Tmp.v1.trns(b.rot()+180,(lenscales[i]-1)*35);
+                /*Tmp.v1.trns(b.rot()+180,(lenscales[i]-1)*35);
                 Lines.stroke((4+Mathf.absin(Time.time(),0.8,1.5))*b.fout()*strokes[s]*tscales[i]);
                 Lines.lineAngle(b.x+Tmp.v1.x,b.y+Tmp.v1.y,b.rot(),baseLen*lenscales[i],CapStyle.none);
                 Tmp.v2.trns(b.rot() + 188, (lenscales[i] - 1) * 35);
@@ -56,7 +54,7 @@ solarflare.shootType = extend(BasicBulletType, {
                 Lines.lineAngle(b.x + Tmp.v2.x, b.y + Tmp.v2.y,b.rot(), baseLen * lenscales[i], CapStyle.none);
                 Tmp.v3.trns(b.rot() + 173, (lenscales[i] - 1) * 35);
                 Lines.stroke((4 + Mathf.absin(Time.time(), 0.8, 1.5)) * b.fout() * strokes[s] * tscales[i]);
-                Lines.lineAngle(b.x + Tmp.v3.x, b.y + Tmp.v3.y, b.rot(), baseLen * lenscales[i], CapStyle.none);
+                Lines.lineAngle(b.x + Tmp.v3.x, b.y + Tmp.v3.y, b.rot(), baseLen * lenscales[i], CapStyle.none);*/
             }
         }
         Draw.reset();
