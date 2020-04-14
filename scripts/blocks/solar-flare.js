1e45093ae288707f9fb44327656d2e5fb8a3f7c0
MEEP of Faith <meepmatthew@gmail.com>
2020-04-14 09:57:52 -0700

    Try this?

--- scripts/blocks/solar-flare.js
+++ scripts/blocks/solar-flare.js
@@ -18,15 +18,22 @@ solarflare.shootType = extend(BasicBulletType, {
         if (b.timer.get(1, 5)){
             //Look in damage.java for how this works, it's simular to lightning.
             Damage.collideLine(b, b.getTeam(), Fx.hitMeltdown, b.x, b.y, b.rot(), length, true);
-            Damage.collideLine(b, b.getTeam(), Fx.hitMeltdown, b.x, b.y, b.rot() +  8, length, true);
-            Damage.collideLine(b, b.getTeam(), Fx.hitMeltdown, b.x, b.y, b.rot() -  8, length, true);
+            /*Damage.collideLine(b, b.getTeam(), Fx.hitMeltdown, b.x, b.y, b.rot() +  8, length, true);
+            Damage.collideLine(b, b.getTeam(), Fx.hitMeltdown, b.x, b.y, b.rot() -  8, length, true);*/
         }
         Effects.shake(1, 1, b.x, b.y);
     },
     hit(b,hitx,hity){
         Effects.effect(this.hitEffect,Color.valueOf("f7d95e"),hitx!=null?hitx:b.x,hity!=null?hity:b.y);
         if(Mathf.chance(0.8)){
-            //TRIPPLE BURNY BURN MWAHAHAHAHAHA
+            //DECUPLE BURNY BURN MWAHAHAHAHAHA
+            Fire.create(Vars.world.tileWorld(hitx + Mathf.range(5), hity + Mathf.range(5)));
+            Fire.create(Vars.world.tileWorld(hitx + Mathf.range(5), hity + Mathf.range(5)));
+            Fire.create(Vars.world.tileWorld(hitx + Mathf.range(5), hity + Mathf.range(5)));
+            Fire.create(Vars.world.tileWorld(hitx + Mathf.range(5), hity + Mathf.range(5)));
+            Fire.create(Vars.world.tileWorld(hitx + Mathf.range(5), hity + Mathf.range(5)));
+            Fire.create(Vars.world.tileWorld(hitx + Mathf.range(5), hity + Mathf.range(5)));
+            Fire.create(Vars.world.tileWorld(hitx + Mathf.range(5), hity + Mathf.range(5)));
             Fire.create(Vars.world.tileWorld(hitx + Mathf.range(5), hity + Mathf.range(5)));
             Fire.create(Vars.world.tileWorld(hitx + Mathf.range(5), hity + Mathf.range(5)));
             Fire.create(Vars.world.tileWorld(hitx + Mathf.range(5), hity + Mathf.range(5)));
@@ -36,20 +43,20 @@ solarflare.shootType = extend(BasicBulletType, {
         baseLen = (length) * b.fout();
 
         Lines.lineAngle(b.x,b.y,b.rot(),baseLen);
-        Lines.lineAngle(b.x, b.y, b.rot() + 8,baseLen);
-        Lines.lineAngle(b.x, b.y, b.rot() - 8,baseLen);
+        /*Lines.lineAngle(b.x, b.y, b.rot() + 8,baseLen);
+        Lines.lineAngle(b.x, b.y, b.rot() - 8,baseLen);*/
         for(var s = 0; s < colors.length; s++){
             Draw.color(tmpColor.set(colors[s]).mul(1+Mathf.absin(Time.time(),1,0.1)));
             for(var i = 0; i < tscales.length; i++){
                 Tmp.v1.trns(b.rot()+180,(lenscales[i]-1)*35);
                 Lines.stroke((4+Mathf.absin(Time.time(),0.8,1.5))*b.fout()*strokes[s]*tscales[i]);
                 Lines.lineAngle(b.x+Tmp.v1.x,b.y+Tmp.v1.y,b.rot(),baseLen*lenscales[i],CapStyle.none);
-                Tmp.v2.trns(b.rot() + 188, (lenscales[i] - 1) * 35);
+                /*Tmp.v2.trns(b.rot() + 188, (lenscales[i] - 1) * 35);
                 Lines.stroke((4 + Mathf.absin(Time.time(), 0.8, 1.5))* b.fout() * strokes[s] * tscales[i]);
                 Lines.lineAngle(b.x + Tmp.v2.x, b.y + Tmp.v2.y,b.rot(), baseLen * lenscales[i], CapStyle.none);
                 Tmp.v3.trns(b.rot() + 173, (lenscales[i] - 1) * 35);
                 Lines.stroke((4 + Mathf.absin(Time.time(), 0.8, 1.5)) * b.fout() * strokes[s] * tscales[i]);
-                Lines.lineAngle(b.x + Tmp.v3.x, b.y + Tmp.v3.y, b.rot(), baseLen * lenscales[i], CapStyle.none);
+                Lines.lineAngle(b.x + Tmp.v3.x, b.y + Tmp.v3.y, b.rot(), baseLen * lenscales[i], CapStyle.none);*/
             }
         }
         Draw.reset();
