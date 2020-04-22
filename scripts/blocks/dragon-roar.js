72364d1d3463ca15ad15ec207bd80ddef0a6dc84
MEEP of Faith <meepmatthew@gmail.com>
2020-04-22 10:59:11 -0700

    fix?

--- scripts/blocks/dragon-roar.js
+++ scripts/blocks/dragon-roar.js
@@ -1,6 +1,5 @@
 const dwago = extendContent(DoubleTurret, "dragon-roar", {
     load(){
-        this.super$load()
         this.baseRegion = Core.atlas.find(this.name + "-base");
     }
 });
--- scripts/blocks/wolfsteeth.js
+++ scripts/blocks/wolfsteeth.js
@@ -1,6 +1,5 @@
 const cutefluffydoggo = extendContent(ItemTurret, "wolfsteeth", {
     load(){
-        this.super$load()
         this.baseRegion = Core.atlas.find(this.name + "-base");
     }
 });
