9c5b81aef0582c739ede4d4c7e88645806d248d2
MEEP of Faith <meepmatthew@gmail.com>
2020-04-23 10:42:41 -0700

    Oversized Weapon Icons

--- scripts/blocks/dragon-roar.js
+++ scripts/blocks/dragon-roar.js
@@ -1,6 +1,12 @@
 const dwago = extendContent(DoubleTurret, "dragon-roar", {
-    load(){
-        this.super$load()
-        this.baseRegion = Core.atlas.find(this.name + "-base");
-    }
+  load(){
+    this.super$load() 
+    this.baseRegion = Core.atlas.find(this.name + "-base");
+  },
+  generateIcons: function(){
+		return [
+			Core.atlas.find("dragon-roar-base-icon"),
+			Core.atlas.find("dragon-roar-icon")
+		];
+	}
 });
--- scripts/blocks/wolfsteeth.js
+++ scripts/blocks/wolfsteeth.js
@@ -1,6 +1,12 @@
 const cutefluffydoggo = extendContent(ItemTurret, "wolfsteeth", {
-    load(){
-        this.super$load()
-        this.baseRegion = Core.atlas.find(this.name + "-base");
-    }
+  load(){
+    this.super$load() 
+    this.baseRegion = Core.atlas.find(this.name + "-base");
+  },
+  generateIcons: function(){
+    return [
+      Core.atlas.find("wolfsteeth-base-icon"),
+      Core.atlas.find("wolfsteeth-roar-icon")
+    ];
+  }
 });
