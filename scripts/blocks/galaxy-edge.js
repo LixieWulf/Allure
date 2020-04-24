003123860dc86d86d2ab7175e3a0a980e137a5d2
MEEP of Faith <meepmatthew@gmail.com>
2020-04-23 18:18:02 -0700

    Revert "Now what'll happen if i readd it"
    This reverts commit 3cc74edceb75277a4e3dc55569b1b5c3f5b7d80b.

--- scripts/blocks/galaxy-edge.js
+++ scripts/blocks/galaxy-edge.js
@@ -6,29 +6,31 @@ var fluidCostMultiplier = 8;
 
 //Editable stuff for custom laser.
 //4 colors from outside in. Normal meltdown laser has trasnparrency 55 -> aa -> ff (no transparrency) -> ff(no transparrency)
-var colors = [/*edge*/Color.valueOf("c90fd955"), Color.valueOf("10bbde55"), Color.valueOf("db410d55"), /*2*/Color.valueOf("d228e0aa"), Color.valueOf("24cef0aa"), Color.valueOf("e65727aa"), /*3*/Color.valueOf("f152ff"), Color.valueOf("3ddfff"), Color.valueOf("ed683b"), /*mid*/Color.white];
+var colors1 = [Color.valueOf("c90fd955"), Color.valueOf("d228e0aa"), Color.valueOf("f152ff"), Color.white];
+var colors2 = [Color.valueOf("10bbde55"), Color.valueOf("24cef0aa"), Color.valueOf("3ddfff"), Color.white];
+var colors3 = [Color.valueOf("db410d55"), Color.valueOf("e65727aa"), Color.valueOf("ed683b"), Color.white];
 //Number of beams
-var lasers = 1;
+var lasers = 5;
 
 //The number of values in the next 4 arrays is the number of beams you have. First values in each go to the first beam, second values go to the second, etc.
 //Beam angles in degrees
-const spread = [0,0];
+const spread = [0, 10, -10, 20, -20];
 //Shift beam left or right. Negative is left, 0 is middle.
-const spacing = [0,0];
+const spacing = [0, 2 ,-2, 4, -4];
 //Shift beam foward or backward. Negative is backward, 0 is middle. Note that it counts from the start of the widest section.
-const position = [0, 0];
+const position = [0, 0, 0, 0 ,0];
 //Length of beam. Uses same 8 per tile rule.
-var length = [160, 160];
+var length = [160, 152, 152, 144, 144];
 
 //Stuff you probably shouldn't edit unless you know what you're doing.
 //Width of each section of the beam from thickest to thinnest
-var tscales = [1, 1, 1, 0.7, 0.7, 0.7, 0.5, 0.5, 0.5, 0.2];
+var tscales = [1, 0.7, 0.5, 0.2];
 //Overall width of each color
-var strokes = [/*edge*/6, 5.5, 5, /*2*/4.5, 4, 3.5 /*3*/3, 2.3, 1.6, /*mid*/0.9];
+var strokes = [6, 4.5, 3, 0.9];
 //Determines how far back each section in the start should be pulled
-var pullscales = [/*edge*/1, 1.02, 1.04, /*2*/1.06, 1.073, 1.086, /*3*/1.1, 1.113, 1.126, /*mid*/1.14];
+var pullscales = [1, 1.06, 1.1, 1.14];
 //Determines how far each section of the end should extend past the main thickest section
-var lenscales = [/*edge*/1, 1.053, 1.106, /*2*/1.16, 1.2, 1.24,/*3*/1.28, 1.32, 1.36, /*mid*/1.4];
+var lenscales = [1, 1.16, 1.27, 1.39];
 
 var tmpColor = new Color();
 const vec = new Vec2();
@@ -58,9 +60,32 @@ galaxyEdge.shootType = extend(BasicBulletType, {
         }
     },
     draw: function(b){
-        for(var s = 0; s < 10; s++){
-            Draw.color(tmpColor.set(colors[s]).mul(1.0 + Mathf.absin(Time.time(), 1.0, 0.3)).shiftHue(Time.time() * 1.5));
-            for(var i = 0; i < 10; i++){
+        
+        for(var s = 0; s < 4; s++){
+            Draw.color(tmpColor.set(colors3[s]).mul(1.0 + Mathf.absin(Time.time(), 1.0, 0.3)));
+            for(var i = 0; i < 4; i++){
+                for(var v = 3; v < 5; v++){
+                    vec.trns(b.rot() - 90, spacing[v], position[v]);
+                    Tmp.v1.trns(b.rot() + angleB + 180.0, (pullscales[i] - 1.0) * 55.0);
+                    var angleB = spread[v];
+                    var baseLen = length[v] * b.fout();
+                    Lines.stroke((4 + Mathf.absin(Time.time(), 0.8, 1.5)) * b.fout() * strokes[s] * tscales[i]);
+                    Lines.lineAngle(b.x + Tmp.v1.x + vec.x, b.y + Tmp.v1.y + vec.y, b.rot() + angleB, baseLen * b.fout() * lenscales[i], CapStyle.none);
+                }
+            }
+            Draw.color(tmpColor.set(colors2[s]).mul(1.0 + Mathf.absin(Time.time(), 1.0, 0.3)));
+            for(var i = 0; i < 4; i++){
+                for(var v = 1; v < 3; v++){
+                    vec.trns(b.rot() - 90, spacing[v], position[v]);
+                    Tmp.v1.trns(b.rot() + angleB + 180.0, (pullscales[i] - 1.0) * 55.0);
+                    var angleB = spread[v];
+                    var baseLen = length[v] * b.fout();
+                    Lines.stroke((4 + Mathf.absin(Time.time(), 0.8, 1.5)) * b.fout() * strokes[s] * tscales[i]);
+                    Lines.lineAngle(b.x + Tmp.v1.x + vec.x, b.y + Tmp.v1.y + vec.y, b.rot() + angleB, baseLen * b.fout() * lenscales[i], CapStyle.none);
+                }
+            }
+            Draw.color(tmpColor.set(colors1[s]).mul(1.0 + Mathf.absin(Time.time(), 1.0, 0.3)));
+            for(var i = 0; i < 4; i++){
                 vec.trns(b.rot() - 90, spacing[0], position[0]);
                 Tmp.v1.trns(b.rot() + angleB + 180.0, (pullscales[i] - 1.0) * 55.0);
                 var angleB = spread[0];
