b2b61918d22b4321ab2dfff0c0b668e0aadd2604
MEEP of Faith <meepmatthew@gmail.com>
2020-04-25 18:53:43 -0700

    blurred laser?

--- scripts/blocks/solar-flare.js
+++ scripts/blocks/solar-flare.js
@@ -36,54 +36,56 @@ solarflare.consumes.add(new ConsumeLiquidFilter(boolf(liquid=>liquid.temperature
 solarflare.coolantMultiplier = 1 / fluidCostMultiplier;
 
 solarflare.shootType = extend(BasicBulletType, {
-    update: function(b){
-        if(b.timer.get(1, 5)){
-            for(var v = 0; v < lasers; v++){
-                vec.trns(b.rot() - 90, spacing[v], position[v]);
-                Tmp.v1.trns(b.rot() + angleB + 180.0, (pullscales[4] - 1.0) * 55.0);
-                var angleB = spread[v];
-                var baseLen = length[v] * b.fout();
-                Damage.collideLine(b, b.getTeam(), this.hitEffect, b.x + vec.x, b.y + vec.y, b.rot() + angleB, length[v] + length[v]/8.75, true);
-            }
-        };
-    },
-    hit(b,hitx,hity){
-        Effects.effect(this.hitEffect,Color.valueOf("f7d95e"),hitx!=null?hitx:b.x,hity!=null?hity:b.y);
-        //Uncomment the following 3 lines to have incend. Chance is 0 to 1. Copy/past the Fire.create line multiple times to create more fire at once.
-        if(Mathf.chance(0.8)){
-            for(ohno = 0; ohno < 69; ohno++) {
-                Damage.createIncend(hitx, hity, 12, 3);
-            }
+  update: function(b){
+    if(b.timer.get(1, 5)){
+        for(var v = 0; v < lasers; v++){
+            vec.trns(b.rot() - 90, spacing[v], position[v]);
+            Tmp.v1.trns(b.rot() + angleB + 180.0, (pullscales[4] - 1.0) * 55.0);
+            var angleB = spread[v];
+            var baseLen = length[v] * b.fout();
+            Damage.collideLine(b, b.getTeam(), this.hitEffect, b.x + vec.x, b.y + vec.y, b.rot() + angleB, length[v] + length[v]/8.75, true);
         }
-    },
-    draw: function(b){
-        
-        
-        for(var s = 0; s < 4; s++){
-            Draw.color(tmpColor.set(colors[s]).mul(1.0 + Mathf.absin(Time.time(), 1.0, 0.3)));
-            for(var i = 0; i < 4; i++){
-                for(var v = 0; v < lasers - 3; v++){
-                    strokes = [2.5, 1.875, 1.25, 0.375];
-                    vec.trns(b.rot() - 90, spacing[v], position[v]);
-                    var angleB = spread[v];
-                    var baseLen = length[v] * b.fout();
-                    Tmp.v1.trns(b.rot() + angleB + 180.0, (pullscales[i] - 1.0) * 55.0);
-                    Lines.stroke((4 + Mathf.absin(Time.time(), 0.8, 1.5)) * b.fout() * strokes[s] * tscales[i]);
-                    Lines.lineAngle(b.x + Tmp.v1.x + vec.x, b.y + Tmp.v1.y + vec.y, b.rot() + angleB, baseLen * b.fout() * lenscales[i], CapStyle.none);
-                }
-                for(var r = lasers - 3; r < lasers; r++){
-                    strokes = [3.75, 2.8125, 1.875, 0.5625];
-                    vec.trns(b.rot() - 90, spacing[r], position[r]);
-                    var angleB = spread[r];
-                    var baseLen = length[r] * b.fout();
-                    Tmp.v1.trns(b.rot() + angleB + 180.0, (pullscales[i] - 1.0) * 55.0);
-                    Lines.stroke((4 + Mathf.absin(Time.time(), 0.8, 1.5)) * b.fout() * strokes[s] * tscales[i]);
-                    Lines.lineAngle(b.x + Tmp.v1.x + vec.x, b.y + Tmp.v1.y + vec.y, b.rot() + angleB, baseLen * b.fout() * lenscales[i], CapStyle.none);
-                }
-            }
-        };
-        Draw.reset();
-    }
+    };
+  },
+  hit(b,hitx,hity){
+      Effects.effect(this.hitEffect,Color.valueOf("f7d95e"),hitx!=null?hitx:b.x,hity!=null?hity:b.y);
+      //Uncomment the following 3 lines to have incend. Chance is 0 to 1. Copy/past the Fire.create line multiple times to create more fire at once.
+      if(Mathf.chance(0.8)){
+          for(ohno = 0; ohno < 69; ohno++) {
+              Damage.createIncend(hitx, hity, 12, 3);
+          }
+      }
+  },
+  draw: function(b){
+      
+      
+      for(var s = 0; s < 4; s++){
+          Draw.blend(Blending.additive);
+          Draw.color(tmpColor.set(colors[s]).mul(1.0 + Mathf.absin(Time.time(), 1.0, 0.3)));
+          for(var i = 0; i < 4; i++){
+              for(var v = 0; v < lasers - 3; v++){
+                  strokes = [2.5, 1.875, 1.25, 0.375];
+                  vec.trns(b.rot() - 90, spacing[v], position[v]);
+                  var angleB = spread[v];
+                  var baseLen = length[v] * b.fout();
+                  Tmp.v1.trns(b.rot() + angleB + 180.0, (pullscales[i] - 1.0) * 55.0);
+                  Lines.stroke((4 + Mathf.absin(Time.time(), 0.8, 1.5)) * b.fout() * strokes[s] * tscales[i]);
+                  Lines.lineAngle(b.x + Tmp.v1.x + vec.x, b.y + Tmp.v1.y + vec.y, b.rot() + angleB, baseLen * b.fout() * lenscales[i], CapStyle.none);
+              }
+              for(var r = lasers - 3; r < lasers; r++){
+                  strokes = [3.75, 2.8125, 1.875, 0.5625];
+                  vec.trns(b.rot() - 90, spacing[r], position[r]);
+                  var angleB = spread[r];
+                  var baseLen = length[r] * b.fout();
+                  Tmp.v1.trns(b.rot() + angleB + 180.0, (pullscales[i] - 1.0) * 55.0);
+                  Lines.stroke((4 + Mathf.absin(Time.time(), 0.8, 1.5)) * b.fout() * strokes[s] * tscales[i]);
+                  Lines.lineAngle(b.x + Tmp.v1.x + vec.x, b.y + Tmp.v1.y + vec.y, b.rot() + angleB, baseLen * b.fout() * lenscales[i], CapStyle.none);
+              }
+          }
+          Draw.blend();
+      };
+      Draw.reset();
+  }
 });
 
 solarflare.shootType.hitEffect = Fx.hitMeltdown;
