d3683a9ca33e24fa8c8caa3dfe071bd70d8327a3
MEEP of Faith <meepmatthew@gmail.com>
2020-04-27 15:23:33 -0700

    -

--- scripts/blocks/bluewolframite-conduit.js
+++ scripts/blocks/bluewolframite-conduit.js
@@ -7,13 +7,13 @@ const bluewolframiteConduit = extendContent(Conduit, "bluewolframite-conduit", {
     }
     //I'm not gonna bother making it not called rainbow.
     for(var i = 0; i < 8; i++){
-      this.pulseReg0[i] = Core.atlas.find(this.name + "rainbow-0-" + i);
-      this.pulseReg1[i] = Core.atlas.find(this.name + "rainbow-1-" + i);
-      this.pulseReg2[i] = Core.atlas.find(this.name + "rainbow-2-" + i);
-      this.pulseReg3[i] = Core.atlas.find(this.name + "rainbow-3-" + i);
-      this.pulseReg4[i] = Core.atlas.find(this.name + "rainbow-4-" + i);
-      this.pulseReg5[i] = Core.atlas.find(this.name + "rainbow-5-" + i);
-      this.pulseReg6[i] = Core.atlas.find(this.name + "rainbow-6-" + i);
+      this.pulseReg0[i] = Core.atlas.find(this.name + "-rainbow-0-" + i);
+      this.pulseReg1[i] = Core.atlas.find(this.name + "-rainbow-1-" + i);
+      this.pulseReg2[i] = Core.atlas.find(this.name + "-rainbow-2-" + i);
+      this.pulseReg3[i] = Core.atlas.find(this.name + "-rainbow-3-" + i);
+      this.pulseReg4[i] = Core.atlas.find(this.name + "-rainbow-4-" + i);
+      this.pulseReg5[i] = Core.atlas.find(this.name + "-rainbow-5-" + i);
+      this.pulseReg6[i] = Core.atlas.find(this.name + "-rainbow-6-" + i);
     }
   },
   drawLayer: function(tile){
@@ -29,49 +29,49 @@ const bluewolframiteConduit = extendContent(Conduit, "bluewolframite-conduit", {
     Draw.color(Color.valueOf("91FFFF").shiftSaturation((Time.time() * 5) + (h * 17)));
     
     if(this.bits[0] == 0){
-      for(var h = 0; h < 6; h++){
+      for(var h = 0; h < 8; h++){
         Draw.rect(this.pulseReg0[h], tile.drawx() + this.tr.x, tile.drawy() + this.tr.y, condRotation - 90);
         Draw.color();
       }
     }
     if(this.bits[0] == 1){
-      for(var h = 0; h < 6; h++){
+      for(var h = 0; h < 8; h++){
         Draw.rect(this.pulseReg1[h], tile.drawx() + this.tr.x, tile.drawy() + this.tr.y, condRotation - 90);
         Draw.color();
       }
     }
     if(this.bits[0] == 2){
-      for(var h = 0; h < 6; h++){
+      for(var h = 0; h < 8; h++){
         Draw.rect(this.pulseReg2[h], tile.drawx() + this.tr.x, tile.drawy() + this.tr.y, condRotation - 90);
         Draw.color();
       }
     }
     if(this.bits[0] == 3){
-      for(var h = 0; h < 6; h++){
+      for(var h = 0; h < 8; h++){
         Draw.rect(this.pulseReg3[h], tile.drawx() + this.tr.x, tile.drawy() + this.tr.y, condRotation - 90);
         Draw.color();
       }
     }
     if(this.bits[0] == 4){
-      for(var h = 0; h < 6; h++){
+      for(var h = 0; h < 8; h++){
         Draw.rect(this.pulseReg4[h], tile.drawx() + this.tr.x, tile.drawy() + this.tr.y, condRotation - 90);
         Draw.color();
       }
     }
     if(this.bits[0] == 5){
-      for(var h = 0; h < 6; h++){
+      for(var h = 0; h < 8; h++){
         Draw.rect(this.pulseReg5[h], tile.drawx() + this.tr.x, tile.drawy() + this.tr.y, condRotation - 90);
         Draw.color();
       }
     }
     if(this.bits[0] == 6){
-      for(var h = 0; h < 6; h++){
+      for(var h = 0; h < 8; h++){
         Draw.rect(this.pulseReg6[h], tile.drawx() + this.tr.x, tile.drawy() + this.tr.y, condRotation - 90);
         Draw.color();
       }
     }
     if(this.bits[0] == 7){
-      for(var h = 0; h < 6; h++){
+      for(var h = 0; h < 8; h++){
         Draw.rect(this.pulseReg7[h], tile.drawx() + this.tr.x, tile.drawy() + this.tr.y, condRotation - 90);
         Draw.color();
       }
