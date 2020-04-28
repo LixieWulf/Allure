441f5f35f53a4039b707f5853ce791f93691b384
MEEP of Faith <meepmatthew@gmail.com>
2020-04-28 10:13:11 -0700

    Day 12398748321759846874326879165732741983294932051843729679324765742169873412834 of waiting for a straight answer to my questions

--- scripts/blocks/bluewolframite-conduit.js
+++ scripts/blocks/bluewolframite-conduit.js
@@ -1,6 +1,7 @@
 const bluewolframiteConduit = extendContent(Conduit, "bluewolframite-conduit", {
   load(){
     this.super$load()
+    
     for(var l = 0; l < this.topRegions.length; l++){
       this.topRegions[l] = Core.atlas.find(this.name + "-top-" + l);
       this.botRegions[l] = Core.atlas.find(this.name + "-bottom-" + l);
@@ -17,24 +18,13 @@ const bluewolframiteConduit = extendContent(Conduit, "bluewolframite-conduit", {
     }
     this.pulseRegAll = [this.pulseReg0, this.pulseReg1, this.pulseReg2, this.pulseReg3, this.pulseReg4, this.pulseReg5, this.pulseReg6]
   },
-  onProximityUpdate(tile){
-    this.super$onProximityUpdate(tile);
-  },
   draw(tile){
-    entity = tile.ent();
-    var rotation = tile.rotation() * 90;
-
-    Draw.colorl(0.34f);
-    Draw.rect(botRegions[entity.blendbits], tile.drawx(), tile.drawy(), rotation);
-
-    Draw.color(tile.entity.liquids.current().color);
-    Draw.alpha(entity.smoothLiquid);
-    Draw.rect(botRegions[entity.blendbits], tile.drawx(), tile.drawy(), rotation);
-    Draw.color();
-
-    Draw.rect(topRegions[entity.blendbits], tile.drawx(), tile.drawy(), rotation);
+    this.super$draw(tile);
     
-    somethingidk = entity.blendbits;
+    entity = tile.ent();
+    rotation = tile.rotation() * 90;
+    
+    somethingidk = 0;
     
     for(var h = 0; h < 8; h++){
 			Draw.color(Color.valueOf("91FFFF").shiftSaturation((Time.time() * 5) + (h * 17)));
