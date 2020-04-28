57162c52e3af043a8a447d6374612c64cb3bfdb0
MEEP of Faith <meepmatthew@gmail.com>
2020-04-28 11:23:16 -0700

    I'm a moron :D

--- scripts/blocks/bluewolframite-conduit.js
+++ scripts/blocks/bluewolframite-conduit.js
@@ -8,15 +8,15 @@ const bluewolframiteConduit = extendContent(Conduit, "bluewolframite-conduit", {
     }
     //I'm not gonna bother making it not called rainbow.
     for(var i = 0; i < 8; i++){
-      pulseReg0[i] = Core.atlas.find(this.name + "-rainbow-0-" + i);
-      pulseReg1[i] = Core.atlas.find(this.name + "-rainbow-1-" + i);
-      pulseReg2[i] = Core.atlas.find(this.name + "-rainbow-2-" + i);
-      pulseReg3[i] = Core.atlas.find(this.name + "-rainbow-3-" + i);
-      pulseReg4[i] = Core.atlas.find(this.name + "-rainbow-4-" + i);
-      pulseReg5[i] = Core.atlas.find(this.name + "-rainbow-5-" + i);
-      pulseReg6[i] = Core.atlas.find(this.name + "-rainbow-6-" + i);
+      this.pulseReg0[i] = Core.atlas.find(this.name + "-rainbow-0-" + i);
+      this.pulseReg1[i] = Core.atlas.find(this.name + "-rainbow-1-" + i);
+      this.pulseReg2[i] = Core.atlas.find(this.name + "-rainbow-2-" + i);
+      this.pulseReg3[i] = Core.atlas.find(this.name + "-rainbow-3-" + i);
+      this.pulseReg4[i] = Core.atlas.find(this.name + "-rainbow-4-" + i);
+      this.pulseReg5[i] = Core.atlas.find(this.name + "-rainbow-5-" + i);
+      this.pulseReg6[i] = Core.atlas.find(this.name + "-rainbow-6-" + i);
     }
-    pulseRegAll = [pulseReg0, pulseReg1, pulseReg2, pulseReg3, pulseReg4, pulseReg5, pulseReg6]
+    this.pulseRegAll = [this.pulseReg0, this.pulseReg1, this.pulseReg2, this.pulseReg3, this.pulseReg4, this.pulseReg5, this.pulseReg6];
   },
   draw(tile){
     this.super$draw(tile);
@@ -24,11 +24,11 @@ const bluewolframiteConduit = extendContent(Conduit, "bluewolframite-conduit", {
     entity = tile.ent();
     rotation = tile.rotation() * 90;
     
-    somethingidk = 0;
+    somethingidk = entity.blendbits;
     
     for(var h = 0; h < 8; h++){
-			Draw.color(Color.valueOf("91FFFF").shiftSaturation((Time.time() * 5) + (h * 17)));
-			Draw.rect(pulseRegAll[entity.blendbits][h], tile.drawx(), tile.drawy(), rotation);
+			Draw.color(Color.valueOf("91FFFF").shiftSaturation((Time.time() * 8) + (h * 17)));
+			Draw.rect(this.pulseRegAll[entity.blendbits][h], tile.drawx(), tile.drawy(), rotation);
 			Draw.color();
 		}
 	}
