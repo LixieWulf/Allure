009dad60b87a3003c076c3704e98baaffd43792a
MEEP of Faith <meepmatthew@gmail.com>
2020-04-28 23:03:26 -0700

    Update solar-flare.js

--- scripts/blocks/solar-flare.js
+++ scripts/blocks/solar-flare.js
@@ -36,13 +36,10 @@ const theSun = newEffect(60, e => {
   Draw.blend(Blending.additive);
 	Draw.color(Color.valueOf("FAFF00"), Color.valueOf("DFE20030"), e.fin());
 	Draw.rect(sunRegion, e.x, e.y, 270);
-	Draw.blend();
-});
-const sunshine = newEffect(20, e => {
-  
   Draw.color(Color.valueOf("fff2009B"), Color.valueOf("ffffa39B"), e.fin());
   Lines.stroke(e.fout() * 8);
-  Lines.circle(e.x, e.y, e.fin() * 18); //draw a circle whose radius goes from 0 to 100
+  Lines.circle(e.x, e.y, e.fin() * 18);
+	Draw.blend();
 });
 
 const solarflare = extendContent(LaserTurret, "solar-flare", {
@@ -64,7 +61,6 @@ const solarflare = extendContent(LaserTurret, "solar-flare", {
       //if(entity.timer.get(1, 10)){
       vec.trns(entity.rotation - 90, 0, -9.5 - entity.recoil);
       Effects.effect(theSun, entity.x + vec.x, entity.y + vec.y, 270);
-      Effects.effect(sunshine, entity.x + vec.x, entity.y + vec.y, 270);
       //}
     }
   }
