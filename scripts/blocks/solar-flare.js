b3d0c0b242fc08daddc509142b14b8326fd324b8
MEEP of Faith <meepmatthew@gmail.com>
2020-04-28 22:53:45 -0700

    work

--- scripts/blocks/solar-flare.js
+++ scripts/blocks/solar-flare.js
@@ -60,7 +60,7 @@ const solarflare = extendContent(LaserTurret, "solar-flare", {
     
     entity = tile.ent();
     
-    if(entity.bulletLife > 0 && entity.bullet != null){
+    if(entity.bullet != null){
       //if(entity.timer.get(1, 10)){
       vec.trns(entity.rotation - 90, 0, -9.5 - entity.recoil);
       Effects.effect(theSun, entity.x + vec.x, entity.y + vec.y, 270);
