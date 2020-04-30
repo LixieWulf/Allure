c93cd735c4fb0f7ad69500c7cfedb978162b4ae9
MEEP of Faith <meepmatthew@gmail.com>
2020-04-30 09:37:16 -0700

    Split effects

--- scripts/blocks/solar-flare.js
+++ scripts/blocks/solar-flare.js
@@ -36,10 +36,14 @@ const theSun = newEffect(60, e => {
   Draw.blend(Blending.additive);
 	Draw.color(Color.valueOf("FAFF00"), Color.valueOf("DFE20030"), e.fin());
 	Draw.rect(sunRegion, e.x, e.y, 270);
+	Draw.blend();
+});
+const sunshine = newEffect(60, e => {
+  Draw.blend(Blending.additive);
   Draw.color(Color.valueOf("fff2009B"), Color.valueOf("ffffa39B"), e.fin());
   Lines.stroke(e.fout() * 8);
   Lines.circle(e.x, e.y, e.fin() * 18);
-	Draw.blend();
+  Draw.blend();
 });
 
 const solarflare = extendContent(LaserTurret, "solar-flare", {
@@ -50,17 +54,11 @@ const solarflare = extendContent(LaserTurret, "solar-flare", {
     entity = tile.ent();
     
     vec.trns(entity.rotation - 90, 0, -9.5 - entity.recoil);
-    Draw.rect(sunSunYee, entity.x + vec.x, entity.y + vec.y, 270);
-  },
-  updateShooting(tile){
-    this.super$updateShooting(tile);
-    
-    entity = tile.ent();
-    
-    if(entity.bulletLife > 0 && entity.bullet != null){
-      vec.trns(entity.rotation - 90, 0, -9.5 - entity.recoil);
-      Effects.effect(theSun, entity.x + vec.x, entity.y + vec.y, 270);
-    }
+    Draw.rect(sunSunYee, entity.x + vec.x, entity.y + vec.y, 0);
+    Effects.effect(theSun, entity.x + vec.x, entity.y + vec.y, 0);
+    if(b.timer.get(1, 10)){
+      Effects.effect(sunshine, entity.x + vec.x, entity.y + vec.y, 0);
+    };
   }
 });
 
